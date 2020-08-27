be3931487ecbe6a0cc8fa48742a3d3405f43d5c8
Azrie Bakri <mohamad.azriebakri@gmail.com>
2020-08-27 21:57:46 +0800

    Added sendGrid

--- contact-form/index.js
+++ contact-form/index.js
@@ -1,50 +1,27 @@
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log(req.body);
     console.log(process.env)
 
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-    //         body: "Hello " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: "Please pass a name on the query string or in the request body"
-    //     };
-    // }
     var data = req.body;
 
-    // emailExists({ sender: process.env.USERNAME, recipient: data.email }).then(console.log)
-    // console.log('Check:', check);
-  
-    var smtpTransport = nodemailer.createTransport({
-      service: 'GMAIL',
-      port: 465,
-      auth: {
-        user: process.env.APPSETTING_SMTPUSERNAME,
-        pass: process.env.APPSETTING_SMTPPASSWORD
-      }
-    });
-
-    var mailOptions = {
-      from: data.email,
+    sgMail.setApiKey(process.env.APPSETTING_SENDGRID_API_KEY);
+    const msg = {
       to: 'mohamad.azriebakri@gmail.com',
+      from: data.email,
       subject: data.subject,
-      html: `<p><b>From: ${data.name}</b> <<i>${data.email}</i>></p><p>${data.message}</p><p>Best regards, ${data.name}</p>`
+      html: `<p><b>From: ${data.name}</b> <<i>${data.email}</i>></p><p>${data.message}</p><p>Best regards, ${data.name}</p>`,
     };
-  
-    smtpTransport.sendMail(mailOptions,
-    (error, response) => {
+
+    sgMail.send(msg, (error, response) => {
       if(error) {
-        context.done(error)
+        context.res(error)
       } else {
 
-        context.done('Success!') 
+        context.res('Success!') 
       }
       smtpTransport.close();
     });
